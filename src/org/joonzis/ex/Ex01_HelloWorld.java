498509af41bd5b6e7949e377d9b87bd139489578
82103 <82103@DESKTOP-6R5M0O7>
2022-09-18 15:15:48 +0900

    initial commit

---
+++ src/org/joonzis/ex/Ex01_HelloWorld.java
@@ -0,0 +1,25 @@
+package org.joonzis.ex;
+
+public class Ex01_HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		
+
+		// 1. //	: 주석만들기 ctrl+/
+		// 2. /*  */: 주석만들기
+		
+		//System.out.println("HelloWorld");
+		/*System.out.println("HelloWorld");*/
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+
+		System.out.println("=====================");
+		
+		System.out.println("HelloWorld");
+	}
+}
